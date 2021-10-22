e4fe1b4b518abba417f9ce750c68c254ae02d24e
Sami_bhr <sami.bouhroum@grenoble-inp.org>
2021-10-23 01:47:29 +0200

    wip

--- hash.h
+++ hash.h
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <openssl/md5.h>
 #include <string.h>
@@ -6,94 +5,97 @@
 #include <math.h>
 #include <stdint.h>
 #include <time.h>
-
-
 #define HASHSIZE 8 // In bytes
-#define SHIFT 3 // In bits
+#define SHIFT 3    // In bits
 typedef uint64_t pwhash;
 #define M 6
 #define NB_PASS_MAX 308915776
 #define R 10
 #define N 100000
-#define L 1000
+#define L 1
 #define BASE 26
 #define HASHSIZE 8 // In bytes
-#define SHIFT 3 // In bits
+#define SHIFT 3    // In bits
 #define MAX_BUFFER 26
 
-pwhash target_hash_function(char *pwd){
-      unsigned char md5[MD5_DIGEST_LENGTH] = {0};
-        MD5((const unsigned char *)pwd, strlen(pwd), md5);
-          char hexamd5hash[2*HASHSIZE+1];
-            hexamd5hash[2*HASHSIZE]='\0';
-              for (int i=0; i < HASHSIZE; i++) {
-                      sprintf(hexamd5hash + 2*i, "%02x", md5[i]);
-                        }
-                pwhash md5hash = strtoull(hexamd5hash, NULL, 16);
-                  pwhash myhash = (md5hash << SHIFT)|(md5hash >> (8*HASHSIZE - SHIFT));
-                    return myhash;
+pwhash target_hash_function(char *pwd)
+{
+  unsigned char md5[MD5_DIGEST_LENGTH] = {0};
+  MD5((const unsigned char *)pwd, strlen(pwd), md5);
+  char hexamd5hash[2 * HASHSIZE + 1];
+  hexamd5hash[2 * HASHSIZE] = '\0';
+  for (int i = 0; i < HASHSIZE; i++)
+  {
+    sprintf(hexamd5hash + 2 * i, "%02x", md5[i]);
+  }
+  pwhash md5hash = strtoull(hexamd5hash, NULL, 16);
+  pwhash myhash = (md5hash << SHIFT) | (md5hash >> (8 * HASHSIZE - SHIFT));
+  return myhash;
 }
 
-
-void init_tableau(char* tab){
-    if(tab == NULL){
-        printf("probleme malloc\n");
-        exit(-1);
-    }
-    for(int i=0; i<M; i++){
-        tab[i] = 0;
-     }
-
-
-
+void init_tableau(char *tab)
+{
+  if (tab == NULL)
+  {
+    printf("probleme malloc\n");
+    exit(-1);
+  }
+  for (int i = 0; i < M; i++)
+  {
+    tab[i] = 0;
+  }
 }
 
+void change_base_function(int number, char *tab)
+{
 
-void change_base_function(int number , char *tab){
+  int quotient = 0;
+  int remainder = 0;
+  int i = M - 1;
 
-    int quotient = 0;
-    int remainder = 0;
-    int i= M - 1;
+  while ((number / BASE) != 0)
+  {
+    tab[i] = (number % BASE);
+    number = number / BASE;
+    i--;
+  }
 
-
-    while((number/BASE) != 0)
-    {
-        tab[i] = (number%BASE);
-        number = number/BASE;
-        i--;
-    }
-
-     tab[i] = (number%BASE) ;
-     for(int i=0; i<M; i++){
-            tab[i]+='a';
-           // printf("tableau %c\n",tab[i]);
-    }
+  tab[i] = (number % BASE);
+  for (int i = 0; i < M; i++)
+  {
+    tab[i] += 'a';
+    // printf("tableau %c\n",tab[i]);
+  }
 }
 
+void reduction_function(uint64_t hash, int columnNumber, char *tab)
+{
 
-
-void reduction_function(uint64_t hash,int columnNumber){
-
-        int modulo_hash = (hash%(NB_PASS_MAX)) + columnNumber;
-        char *tab= malloc(sizeof(char)*M);
-        init_tableau(tab);
-        change_base_function(modulo_hash,tab);
-        printf("Mon mots de passe est : %s\n",tab);
-
+  int modulo_hash = (hash % (NB_PASS_MAX)) + columnNumber;
+  // char *tab= malloc(sizeof(char)*M);
+  //init_tableau(tab);
+  change_base_function(modulo_hash, tab);
+  // printf("Mon mots de passe est : %s\n",tab);
 }
 
+void generate_pwd(char *pwd)
+{
 
-void generate_pwd(char *pwd){
-  
-   // seed
-   srand( time (NULL));
-   for (int i=0;i<L;i++){
-      pwd[i]=rand()%26 + 'a';
-   }
+  // seed
+  srand(time(NULL));
+  for (int i = 0; i < M; i++)
+  {
+    pwd[i] = rand() % 26 + 'a';
+  }
 }
 
-
-
-
-
-
+void generate_chain(char *pwd)
+{
+  uint64_t hash;
+  for (int i = 0; i < L; i++)
+  {
+    printf("tema le mdp : %s ", pwd);
+    hash = target_hash_function(pwd);
+    reduction_function(hash, i, pwd);
+  }
+}
--- main.c
+++ main.c
@@ -10,11 +10,29 @@ int main (int argc, char* argv[])
 {
 //    printf("%"PRIu64 "\n", target_hash_function("arvind"));
    // int tab[6] = {0, 4, 8, 9, 6};
-   reduction_function(486486545,1);
+//   reduction_function(486486545,1);
+   char* tab=malloc(sizeof(char)*6);
+   init_tableau(tab);
+   tab[0]='w';
+   tab[1]='y';
+   tab[2]='u';
+   tab[3]='u';
+   tab[4]='r';
+   tab[5]='o';
+
+
+//   generate_pwd(tab);
+   printf("clès : %s - ", tab);
+   free(tab);
+   generate_chain(tab) ;
+   printf("%s\n", tab);
+
+    printf("wyuuro ");
+    uint64_t  hash;
+    hash=target_hash_function("wyuuro ");
+
+    reduction_function(hash,0,tab);
+    printf("%s\n", tab);
  //   printf("apres appel ");
 }
-
-
-
-
 
