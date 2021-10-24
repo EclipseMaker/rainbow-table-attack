0b8391d778a08bab0fc3a47e4a645c2c0282984a
Sami_bhr <sami.bouhroum@grenoble-inp.org>
2021-10-24 22:15:29 +0200

    hash_many_last_version

--- hash_many.c
+++ hash_many.c
@@ -1,47 +1,46 @@
-#include "hash.h"
-#include <openssl/md5.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "hash.h"
 
-int writeHash(FILE *pwdList, FILE *destFile) {
-  char *tab = malloc(sizeof(char) * M);
-  char c;
-  int cpt = 1;
-  while (true) {
-    int i = 0;
-    while (i <= M) {
-      tab[i] = fgetc(pwdList);
-      i++;
-      if (feof(pwdList))
-        return cpt;
+int writeHash(FILE* pwdList, FILE* destFile)
+{
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int cpt = 0;
+    while ((read = getline(&line, &len, pwdList)) != -1)
+    {
+        fprintf(destFile,"%lu\n", target_hash_function(line));
+        cpt++;
     }
-    fprintf(destFile, "%lu\n", target_hash_function(tab));
-    cpt++;
-  }
 
-  return 0;
+    return cpt;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    fprintf(stderr, "Please give at least a password file and a destination "
-                    "file for hashes");
-    return -1;
-  }
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "Please give at least a password file and a destination file for hashes");
+        return -1;
+    }
 
-  FILE *pwdList = fopen(argv[1], "r");
-  if (pwdList == NULL) {
-    fprintf(stderr, "cannot open file %s\n", argv[1]);
-    return -1;
-  }
+    FILE* pwdList = fopen(argv[1], "r");
+    if (pwdList == NULL)
+    {
+        fprintf(stderr,"cannot open file %s\n",argv[1]);
+        return -1;
+    }
 
-  FILE *destFile = fopen(argv[2], "w");
-  if (destFile == NULL) {
-    fprintf(stderr, "cannot open file %s\n", argv[1]);
-    return -1;
-  }
-  printf("%d hashes been written in the following file : %s \n",
-         writeHash(pwdList, destFile), argv[2]);
-  return 0;
+    FILE* destFile = fopen(argv[2], "w");
+    if (destFile == NULL)
+    {
+        fprintf(stderr,"cannot open file %s\n",argv[1]);
+        return -1;
+    }
+
+    printf("%i hashes been written in the following file : %s \n", writeHash(pwdList, destFile), argv[2]);
+    return 0;
 }
+
